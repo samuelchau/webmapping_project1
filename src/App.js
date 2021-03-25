0512bb25905088a6ba49da58c5e34fc833ff24ac
Samuel Chau <samuel.chau@mail.mcgill.ca>
2021-03-25 15:08:12 -0400

    Added a new Grid component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
+import MyFirstGrid from './components/MyFirstGrid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyFirstGrid/>
     </div>
   );
 }
